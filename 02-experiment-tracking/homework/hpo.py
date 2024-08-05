3e538a76ea93c90956d4399d674e8b92c1ebca08
reallaboroius <reallaboroius@gmail.com>
2024-08-05 12:19:20 +0200

    finished homework of the 2nd module

--- 02-experiment-tracking/homework/hpo.py
+++ 02-experiment-tracking/homework/hpo.py
@@ -7,6 +7,7 @@ from hyperopt import STATUS_OK, Trials, fmin, hp, tpe
 from hyperopt.pyll import scope
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_squared_error
+import requests
 
 # mlflow.set_tracking_uri("http://127.0.0.1:5000")
 # mlflow.set_experiment("random-forest-hyperopt")
@@ -17,11 +18,26 @@ def load_pickle(filename: str):
         return pickle.load(f_in)
 
 
-def run_optimization(data_path: str, num_trials: int = 15,server_uri: str = "http://127.0.0.1:5000",depth_from: int = 10, depth_to: int = 20,experiment_prefix: str = 'max_depth'):
+def run_optimization(data_path: str, num_trials: int = 15,server_uri: str = "http://127.0.0.1:5000",depth_from: int = 1, depth_to: int = 20,experiment_prefix: str = 'max_depth'):
 
+    response = requests.get(server_uri, timeout=10)  # Request the URI and set a timeout, e.g., 10 seconds
+    # Check if the server returned a successful status code (200 range)
+    if response.status_code == 200:
+        print("URI is available:", server_uri)
+    else:
+        print("Server responded, but URI might not be available, Status code:", response.status_code)
+    
     experiment_name = f'{experiment_prefix}_{depth_from}_{depth_to}_{num_trials}'
     mlflow.set_tracking_uri(server_uri)
-    mlflow.set_experiment(experiment_name)
+    iterator=1
+    experiment_name_final=experiment_name
+    while mlflow.get_experiment_by_name(experiment_name_final):
+        print(f'There exist an experiment {experiment_name_final}, checking ',end='')
+        iterator+=1
+        experiment_name_final=f'{experiment_name}_i{iterator}'
+        print(experiment_name_final)
+    # print(f'Using {experiment_name}')
+    mlflow.set_experiment(experiment_name_final)
 
 
     X_train, y_train = load_pickle(os.path.join(data_path, "train.pkl"))
@@ -66,10 +82,21 @@ def run_optimization(data_path: str, num_trials: int = 15,server_uri: str = "htt
     help="Location where the processed NYC taxi trip data was saved"
 )
 @click.option(
+    "--server_uri",
+    help="The URI or the MLFlow server to connect to"
+)
+@click.option(
+    "--experiment_prefix",
+    help="The experiment name prefix"
+)
+@click.option(
     "--num_trials",
     help="The number of parameter evaluations for the optimizer to explore"
 )
-def run_optimization_cli(**kwargs): print(run_optimization(**kwargs))
+def run_optimization_cli(**kwargs):
+    for i in kwargs.copy():
+        if not kwargs[i]: del kwargs[i]
+    print(run_optimization(**kwargs))
 
 if __name__ == '__main__':
     run_optimization_cli()
